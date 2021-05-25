324b714af0a4dd806ddcb84049f8ca305eb906ee
ViveDevelopment <vivedevelopmentt@gmail.com>
2021-05-25 04:03:31 +0300

    Improved README.md and enable / disable function

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Simple React Clickaway Hook
+
+Simple react `useClickAway` hook, which listens for clicks outside the element.
+
+## Installation
+
+```
+npm i simple-react-clickaway
+```
+
+Import hook
+
+```js
+import useClickAway from 'simple-react-clickaway';
+```
+
+And use it like:
+
+```js
+const {disable, enable} = useClickAway(ref, onClickAway);
+```
--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,14 @@ export const useClickAway = (ref: RefObject<HTMLDivElement>, onClickAway: () => 
   const disable = () => setDisabled(true);
   const enable = () => setDisabled(false);
   const onAway = (e: MouseEvent) => {
-    if (disabled) return;
     if (ref.current && !ref.current.contains(e.target as Element)) onClickAway();
   };
   useEffect(() => {
     if (!ref || !ref.current) return;
-    document.addEventListener('mousedown', onAway);
+    if (disabled)
+      document.removeEventListener('mousedown', onAway);
+    else
+      document.addEventListener('mousedown', onAway);
     return () => document.removeEventListener('mousedown', onAway);
   }, [ref.current, disabled]);
   return { disable, enable };
