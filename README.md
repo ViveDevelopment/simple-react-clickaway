0a2b5a0455acbe10fde9fb4fd9b526f50c2a5997
ViveDevelopment <vivedevelopmentt@gmail.com>
2021-05-26 16:40:07 +0300

    Added example to README.md. Added code docs. Added any html element support.

--- README.md
+++ README.md
@@ -19,3 +19,30 @@ And use it like:
 ```js
 const {disable, enable} = useClickAway(ref, onClickAway);
 ```
+
+## Example
+
+Here is a simple example of detecting a click outside a modal window:
+
+```js
+import "./styles.css";
+import { useClickAway } from "simple-react-clickaway";
+import { useRef } from "react";
+
+export default function App() {
+  const modalRef = useRef();
+  const handleClickAway = () => {
+    alert("You clicked away!");
+  };
+  const { disable, enable } = useClickAway(modalRef, handleClickAway);
+  return (
+    <div className="App">
+      <div className="modal" ref={modalRef}>
+        <button onClick={disable}>Disable click away</button>
+        <button onClick={enable}>Enable click away</button>
+      </div>
+    </div>
+  );
+}
+```
+[View it on a code sandbox](https://codesandbox.io/s/simple-react-clickaway-qduck)
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
 import { RefObject, useEffect, useState } from 'react';
 
+/**
+ * Listens for clicks outside the element.
+ * @property disable Function to temporarily disable click away listener.
+ * @property enable Function to enable click away listener that was previously disabled.
+ */
 type ClickAwayListener = {
   disable: () => void;
   enable: () => void;
 };
 
-export const useClickAway = (ref: RefObject<HTMLDivElement>, onClickAway: () => void): ClickAwayListener => {
+/**
+ * Listens for clicks outside the element.
+ * @param ref Target html element ref.
+ * @param onClickAway Click away callback.
+ * @return Listener disable and enable function.
+ */
+export const useClickAway = (ref: RefObject<HTMLElement>, onClickAway: () => void): ClickAwayListener => {
   const [disabled, setDisabled] = useState<boolean>(false);
   const disable = () => setDisabled(true);
   const enable = () => setDisabled(false);
-  const onAway = (e: MouseEvent) => {
+  const onAway = (e: MouseEvent | PointerEvent | TouchEvent) => {
     if (ref.current && !ref.current.contains(e.target as Element)) onClickAway();
   };
+  const events: ('mousedown' | 'pointerdown' | 'touchstart')[] = ['mousedown', 'pointerdown', 'touchstart'];
+  const addEventListeners = () => events.forEach(event => document.addEventListener(event, onAway));
+  const removeEventListeners = () => events.forEach(event => document.removeEventListener(event, onAway));
   useEffect(() => {
     if (!ref || !ref.current) return;
-    if (disabled) document.removeEventListener('mousedown', onAway);
-    else document.addEventListener('mousedown', onAway);
-    return () => document.removeEventListener('mousedown', onAway);
+    if (disabled) removeEventListeners();
+    else addEventListeners();
+    return removeEventListeners;
   }, [ref.current, disabled]);
   return { disable, enable };
 };
